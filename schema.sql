b71156e4dea17cfbb59a130412d2230220ac95d6
James Serengia <jamesserengia@gmail.com>
2023-03-29 12:33:18 +0300

    Updated animals species

--- schema.sql
+++ schema.sql
@@ -12,5 +12,17 @@
 
 --update table col
 ALTER TABLE animals ADD COLUMN species VARCHAR(50);
+
+-- BEGIN; COMMIT; ROLLBACK; transactions
+BEGIN;
+ALTER TABLE animals RENAME COLUMN species TO unspecified;
+ROLLBACK;
+
+BEGIN;
+UPDATE animals SET species = 'digimon' WHERE name LIKE '%mon';
+UPDATE animals SET species = 'pokemon' WHERE species IS NULL;
+COMMIT;
+
+
         
 
