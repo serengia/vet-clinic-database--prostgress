d92ad32d6a519f785858c1f4c80c8ff48cea9bd1
James Serengia <jamesserengia@gmail.com>
2023-03-29 14:28:56 +0300

    Implemented advanced queries with aggregations

--- queries.sql
+++ queries.sql
@@ -26,3 +26,16 @@ UPDATE animals SET weight_kg = weight_kg*-1;
 ROLLBACK TO "sp1";
 UPDATE animals SET weight_kg = weight_kg*-1 WHERE weight_kg <0;
 COMMIT;
+
+-- STATISTICAL DATA -- aggregation
+SELECT COUNT(*) FROM animals;
+SELECT COUNT(*) FROM animals WHERE escape_attempts = 0;
+SELECT AVG(weight_kg) FROM animals;
+SELECT neutered, MAX(escape_attempts) FROM animals GROUP BY neutered
+ORDER BY neutered DESC LIMIT 1;
+SELECT species, MIN(weight_kg), MAX(weight_kg) FROM animals GROUP BY species;
+SELECT species, AVG(escape_attempts) FROM animals GROUP BY species;
+
+SELECT species, AVG(escape_attempts) FROM animals 
+WHERE date_of_birth BETWEEN '1990-01-01' AND '2000-12-31'
+GROUP BY species;
