35402c18b4e426422a819b09ce43603e0367f23e
James Serengia <jamesserengia@gmail.com>
2023-03-29 11:23:15 +0300

    Add a 'species' column and updated the animals table

--- schema.sql
+++ schema.sql
@@ -1,6 +1,6 @@
 /* Database schema to keep the structure of entire database. */
 
-
+--Create table
     CREATE TABLE animals(
         id BIGSERIAL PRIMARY KEY,
         name VARCHAR(100) NOT NULL, 
@@ -9,5 +9,8 @@
         neutered BOOLEAN NOT NULL, 
         weight_kg DECIMAL NOT NULL
         )
+
+--update table col
+ALTER TABLE animals ADD COLUMN species VARCHAR(50);
         
 
